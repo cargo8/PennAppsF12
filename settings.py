1e3d8eefa0c862db6641976cf897a7a40d6af199
Josh Wilson <wilson.joshual@gmail.com>
2012-09-15 16:52:55 -0400

    prepared for heroku

--- settings.py
+++ settings.py
@@ -4,11 +4,14 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Jason Mow', 'jason.mow@gmail.com'),
+    ('Josh Wilson', 'jason.mow@gmail.com'),
 )
 
 MANAGERS = ADMINS
-
+import dj_database_url
+DATABASES = {'default':
+             dj_database_url.config(default='postgres://localhost')}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -20,7 +23,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
+"""
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
