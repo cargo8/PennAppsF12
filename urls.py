a3a2e115721a18816167c7e879ddc5b0e4eed9aa
Jason Mow <jason.mow@gmail.com>
2012-09-16 09:15:41 -0400

    logout after any url

--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^signup/$', signup),
     url(r'^register/$', register),
     url(r'^login/$', login),
-    url(r'^logout/$', logout),
+    url(r'logout/$', logout),
     url(r'^receiveEmail/$', receiveEmail),
     url(r'^talks/*', talks),
 
